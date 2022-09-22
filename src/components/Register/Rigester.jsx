22120e6b59805bba6a5c818ee740bba86ebcdebe
mohamedmustafa211 <afandymohamed4@gmail.com>
2022-09-22 03:59:56 +0200

    muhamed

--- src/components/Register/Rigester.jsx
+++ src/components/Register/Rigester.jsx
@@ -1,119 +1,121 @@
 import { React, useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 // import { createBrowserHistory } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+function Rigester() {
+  // localStorage.clear()
 
+  const navigate = useNavigate();
 
-function Register() {
-    const navigate = useNavigate();
+  // let history = createBrowserHistory();
+  // console.log(history)
 
-    // let history = createBrowserHistory();
-    // console.log(history)
+  let [newUser, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+  function getUser(e) {
+    let myUser = { ...newUser };
+    myUser[e.target.name] = e.target.value;
+    setUser(myUser);
+  }
+  const [errorReg, seterror] = useState([]);
+ 
+console.log(errorReg)
+
+  //  const navigate = useNavigate();
+ 
+
+  async function FormSubmit(e) {
+    axios.interceptors.response.use(undefined, (error) => {
+        const erorrDataa = error.response.data;
+        seterror(erorrDataa)
+
+        // if(erorrDataa.message ==='Success' ){
+        //     console.log('gotit')
+        // }
     
+      });
+    e.preventDefault();
+    const res = await axios.post("http://127.0.0.1:8000/api/register", newUser);
+    const json = res.data;
+    // seterror(json)
+    if (json.message === 'Success') {
+      navigate("/login");
 
-    let [newUser, setNewUser] = useState({ name: "", email: "",password:"",password_confirmation:"" });
-    function getUser(e) {
-        let myUser = { ...newUser };
-        myUser[e.target.name] = e.target.value;
-        setNewUser(myUser);
-        // console.log(myUser)
+    } else {
+       console.log('helllo')
+      console.log(errorReg)
     }
-                
-    
-                    //  const navigate = useNavigate();
+  }
 
-   
+  return (
+    <div className="container">
+      <div className="row ">
+        {/* <h2>You Have To Login To See Home Page</h2> */}
+        <div className="parent">
+          <div className="form-div">
+            
+            <form onSubmit={FormSubmit} className="w-50   ">
+              <div className="my-2">
+              <div>
+              {errorReg.message ? (
+                <div className="alert-danger rounded-3 py-3 m-2 p-3">{errorReg.message}</div>
+              ) : (
+                <></>
+              )}
+            </div>
+                <label htmlFor="email">Your Name :</label>
+                <input
+                  onChange={getUser}
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  id="nameInp"
+                />
+              </div>
+              <div className="my-2">
+                <label htmlFor="email">Your Email :</label>
+                <input
+                  onChange={getUser}
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="emailInp"
+                />
+              </div>
+              <div className="my-2">
+                <label htmlFor="email">Your Password :</label>
+                <input
+                  onChange={getUser}
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  id="passwordInp"
+                />
+              </div>
+              <div className="my-2">
+                <label htmlFor="password">password confirmation :</label>
+                <input
+                  onChange={getUser}
+                  type="password"
+                  className="form-control"
+                  name="password_confirmation"
+                  id="password_confirmation"
+                />
+              </div>
 
-
-    async function FormSubmit(e) {
-        e.preventDefault();
-        const res = await axios.post("http://127.0.0.1:8000/api/register",newUser);
-        const json =  res.data
-        // const history = useNavigate();
-       
-        
-        // console.log(json)
-
-        if(json.message === 'Success'){
-            localStorage.setItem("email", res.data.user.email);
-            // console.log(localStorage)
-            navigate("/login");
-        
-            // history('/home');
-            // console.log(history)
-            // navigate('/home');
-        }else{
-        //    console.log('heelooo')
-        }
-    
-    }
-
-        
-        
-        return (
-        <div className="container">
-            <div className="row ">
-
-                            {/* <h2>You Have To Login To See Home Page</h2> */}
-            <div className="parent">
-            <div className="form-div">
-                
-                <form onSubmit={FormSubmit}className='w-50   '>
-                
-
-                <div className="my-2">
-                    <label htmlFor="email">Your Name :</label>
-                    <input
-                    onChange={getUser}
-                    type="text"
-
-                        className="form-control"
-                        name="name"
-                        id="nameInp"
-                    />
-                </div>
-                <div className="my-2">
-                    <label htmlFor="password">Your email :</label>
-                    <input
-                    onChange={getUser}
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        id="emailInp"
-                    />
-                </div>
-                <div className="my-2">
-                    <label htmlFor="password">password :</label>
-                    <input
-                    onChange={getUser}
-                        type="password"
-                        className="form-control"
-                        name="password"
-                        id="passInp"
-                    />
-                </div>
-                <div className="my-2">
-                    <label htmlFor="password">password confirmation :</label>
-                    <input
-                    onChange={getUser}
-                        type="password"
-                        className="form-control"
-                        name="password_confirmation"
-                        id="confirmPassInp"
-                    />
-                </div>
-
-                <button type="submit " className="btn btn-info ">Register</button>
+              <button type="submit " className="btn btn-info ">
+                Login
+              </button>
             </form>
-                </div>
-            </div>
-           
-               
-            </div>
-            </div>
-          
-       
-    );
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register;
+export default Rigester;
